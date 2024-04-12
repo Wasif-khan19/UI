7581fc1edf39b297008441d284b62e39c2c79452
Wasif <wk5599426@gmail.com>
2024-04-13 02:22:06 +0500

    edited

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -25,7 +25,6 @@ const Page = () => {
   }: TAuthCredentialsValidator) => {
     mutate({email, password})
   };
-
   return (
     <>
       <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
